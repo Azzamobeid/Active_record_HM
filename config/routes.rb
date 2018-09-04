a167f093b1e91bce426c360e807ce68b9f7e02ba
Azzam Obeid <azzam3bied@gmail.com>
2018-09-03 21:10:29 -0500

    addition

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :comments
   resources :posts
-  
+
   get 'index' => 'practice#index'
   post 'index' => 'practice#index'
   get 'about' => 'practice#about'
